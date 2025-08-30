005ffe1104c046790322decfbeb657c8c63f07d7
Kegnar <mitriy.exe@gmail.com>
2025-08-30 20:51:03 +0400

    Создан метод add

---
+++ Lesson_8_EntityFW_DBFirst/Program.cs
@@ -0,0 +1,25 @@
+/*
+ * Entity FW
+ * Объектная модель - 
+ * Object Relation Mapping -
+ * Entity Data Model
+ *      Conceptual Model - классы и их взаимоотношения
+ *      Mapping - инфо 
+ *      Storage Model - инфо о структуре БД
+ *      file.edmx - xaml файл с описанием
+ *  LINQ to Entities расширение доступа к концептуальной модели
+ *  Entity SQL 
+ *
+ *  Способы:
+ *      DBFirst - БД -> EDM -> Сущность
+ *      
+ */
+namespace Lesson_8_EntityFW_DBFirst
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
